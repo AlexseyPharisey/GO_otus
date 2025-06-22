9e725b8d06c71b261dc0ec766643c024e2e05eb5
Lexach <79162875844@mail.ru>
2025-06-22 20:30:55 +0300

    go-otus system_stats 2

--- statistics/main.go
+++ statistics/main.go
@@ -1,81 +1,102 @@
 package main
 
 import (
-	"GO_otus/statistics/settings"
-	"fmt"
-	"math"
-	"runtime"
-	"strings"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"time"
 )
 
-const bytesInGB = 1024 * 1024 * 1024
-
 func main() {
-	os := runtime.GOOS
+	testMetric := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "test27",
+		},
+		[]string{"label"},
+	)
+	prometheus.MustRegister(testMetric)
 
-	keys := []string{"pc", "os", "bios", "cpu", "ram", "disk", "main", "gru", "net"}
-	titles := map[string]string{
-		"pc":   "Данные ПК:",
-		"os":   "Система:",
-		"bios": "BIOS:",
-		"cpu":  "Процессор:",
-		"ram":  "Оперативная память:",
-		"disk": "HDD/SSD:",
-		"main": "Материнская плата:",
-		"gru":  "Видеокарта:",
-		"net":  "Сетевые настройки:",
-	}
-
-	if os == "windows" {
-		result := settings.GetSystemInfo()
-		for _, key := range keys {
-			value, ok := result[key]
-			if !ok {
-				continue
-			}
-			fmt.Printf(titles[key] + "\n")
-
-			switch systemInfoData := value.(type) {
-			case string:
-				fmt.Printf(" - %s\n\n", systemInfoData)
-			case []map[string]any:
-				for _, item := range systemInfoData {
-					for mapKey, mapVal := range item {
-						if mapKey == "Capacity" || mapKey == "Size" || mapKey == "AdapterRAM" {
-							memoryInGb := math.Round(mapVal.(float64) / bytesInGB)
-							fmt.Printf("  - %s: %v GB\n", mapKey, memoryInGb)
-							continue
-						}
-						fmt.Printf("  - %s: %v\n", mapKey, mapVal)
-					}
-					fmt.Println()
-				}
-			case map[string]any:
-				for mapKey, mapVal := range systemInfoData {
-					if mapKey == "Capacity" || mapKey == "Size" || mapKey == "AdapterRAM" {
-						memoryInGb := math.Round(mapVal.(float64) / bytesInGB)
-						fmt.Printf("  - %s: %v GB\n", mapKey, memoryInGb)
-						continue
-					}
-					if key == "os" && mapKey == "Caption" {
-						osName := mapVal.(string)
-						osName = osName[strings.IndexByte(osName, 'W'):]
-						fmt.Printf("  - %s: %v\n", mapKey, osName)
-						continue
-					}
-					fmt.Printf("  - %s: %v\n", mapKey, mapVal)
-				}
-				fmt.Println()
-			default:
-				fmt.Printf("  %v\n\n", systemInfoData)
-			}
+	go func() {
+		for {
+			testMetric.WithLabelValues("value1").Inc()
+			time.Sleep(1 * time.Second)
 		}
-	}
+	}()
 
-	if os == "linux" {
-		fmt.Println(GetAnalyticsLinux())
+	http.Handle("/metrics", promhttp.Handler())
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
 	}
 }
+
+//func main() {
+//os := runtime.GOOS
+//
+//keys := []string{"pc", "os", "bios", "cpu", "ram", "disk", "main", "gru", "net"}
+//titles := map[string]string{
+//	"pc":   "Данные ПК:",
+//	"os":   "Система:",
+//	"bios": "BIOS:",
+//	"cpu":  "Процессор:",
+//	"ram":  "Оперативная память:",
+//	"disk": "HDD/SSD:",
+//	"main": "Материнская плата:",
+//	"gru":  "Видеокарта:",
+//	"net":  "Сетевые настройки:",
+//}
+//
+//if os == "windows" {
+//	result := settings.GetSystemInfo()
+//	for _, key := range keys {
+//		value, ok := result[key]
+//		if !ok {
+//			continue
+//		}
+//		fmt.Printf(titles[key] + "\n")
+//
+//		switch systemInfoData := value.(type) {
+//		case string:
+//			fmt.Printf(" - %s\n\n", systemInfoData)
+//		case []map[string]any:
+//			for _, item := range systemInfoData {
+//				for mapKey, mapVal := range item {
+//					if mapKey == "Capacity" || mapKey == "Size" || mapKey == "AdapterRAM" {
+//						memoryInGb := math.Round(mapVal.(float64) / bytesInGB)
+//						fmt.Printf("  - %s: %v GB\n", mapKey, memoryInGb)
+//						continue
+//					}
+//					fmt.Printf("  - %s: %v\n", mapKey, mapVal)
+//				}
+//				fmt.Println()
+//			}
+//		case map[string]any:
+//			for mapKey, mapVal := range systemInfoData {
+//				if mapKey == "Capacity" || mapKey == "Size" || mapKey == "AdapterRAM" {
+//					memoryInGb := math.Round(mapVal.(float64) / bytesInGB)
+//					fmt.Printf("  - %s: %v GB\n", mapKey, memoryInGb)
+//					continue
+//				}
+//				if key == "os" && mapKey == "Caption" {
+//					osName := mapVal.(string)
+//					osName = osName[strings.IndexByte(osName, 'W'):]
+//					fmt.Printf("  - %s: %v\n", mapKey, osName)
+//					continue
+//				}
+//				fmt.Printf("  - %s: %v\n", mapKey, mapVal)
+//			}
+//			fmt.Println()
+//		default:
+//			fmt.Printf("  %v\n\n", systemInfoData)
+//		}
+//	}
+//}
+//
+//if os == "linux" {
+//	fmt.Println(GetAnalyticsLinux())
+//}
+// Создаем метрику
+//}
 
 func prepareSystemData(data map[string]any) {
 
